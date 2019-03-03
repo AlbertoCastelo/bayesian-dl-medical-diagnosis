aeddbb78587877b64d9df40c5019a0f9dcb711b4
Alberto Castelo <alberto.castelo@nextail.co>
2019-03-03 17:06:43 +0100

    add main structure

---
+++ deep_GP/main.py
@@ -0,0 +1,18 @@
+import torch
+
+use_cuda = torch.cuda.is_available()
+torch.cuda.set_device(0)
+
+
+def test():
+    pass
+
+
+def train():
+    pass
+
+
+if __name__ == '__main__':
+    train()
+
+    test()
