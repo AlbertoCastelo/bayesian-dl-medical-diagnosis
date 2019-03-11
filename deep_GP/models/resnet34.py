b178247604ee6e1430fdaacaee5d22d5b309c7da
Alberto Castelo <alberto.castelo@nextail.co>
2019-03-11 09:43:46 +0100

    add resnet34 model to be implemented

---
+++ deep_GP/models/resnet34.py
@@ -0,0 +1,10 @@
+from torch import nn
+
+
+class ResNet34(nn.Module):
+    def __init__(self, n_channels=3, drop_rate=0):
+        pass
+
+class ResNet34FeatureExtractor(ResNet34):
+    def forward(self, x):
+        pass
